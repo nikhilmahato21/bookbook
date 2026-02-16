581e6a10cdd14783c18821d495a226bf4c070a77
nikhilmahato21 <nikhilmahato3337@gmail.com>
2026-02-17 00:18:58 +0530

    express change

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.send("hello world!")
+    res.send("hello human!! ")
 })
 
 app.post('/signup',async (req,res)=>{
